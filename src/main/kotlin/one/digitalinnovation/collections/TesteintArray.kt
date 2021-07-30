ecd9d3207ec5b336c7bfe9587fdebd1b790d96e6
Wil-Zeledon <wzeledon@ug.uchile.cl>
2021-07-30 20:10:03 -0300

    Created int array test class

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteintArray.kt
@@ -0,0 +1,41 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val values = IntArray(size = 5)
+    values [0] = 1
+    values [1] = 7
+    values [2] = 6
+    values [3] = 3
+    values [4] = 2
+
+    println("\n")
+    println("A\n")
+    for (valor in values) {
+        println(valor)
+    }
+
+    println("\n")
+    println("B\n")
+        values.forEach {
+            println(it)
+        }
+
+    println("\n")
+    println("C\n")
+    values.forEach { valor ->
+        println(valor)
+    }
+
+    println("\n")
+    println("D\n")
+    for (index in values.indices) {
+        println(values  [index])
+    }
+
+    println("\n")
+    println("E\n")
+    values.sort()                       //FOR ORDER THE LIST
+    for (valor in values) {
+        println(valor)
+    }
+}
