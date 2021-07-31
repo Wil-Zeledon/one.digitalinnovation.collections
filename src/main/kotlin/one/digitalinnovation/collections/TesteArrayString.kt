595a120facc1d6b27966c878e7dbb035ab952e41
Wil-Zeledon <wzeledon@ug.uchile.cl>
2021-07-30 21:57:56 -0300

    Created int array test class

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteArrayString.kt
@@ -0,0 +1,43 @@
+package one.digitalinnovation.collections
+
+fun main() {
+
+    val names = Array<String>(size = 3) {""}
+    names [0] = "MARIA"
+    names [1] = "ZAZA"
+    names [2] = "JOSE"
+
+
+    println("\n")
+    println("A\n")
+    for (name in names) {
+        println(name)
+    }
+
+    println("\n")
+    println("B\n")
+    names.forEach {
+        println(it)
+    }
+
+    println("\n")
+    println("C\n")
+    names.forEach { name ->
+        println(name)
+    }
+
+    println("\n")
+    println("D\n")
+    for (index in names.indices) {
+        println(names  [index])
+    }
+
+    println("\n")
+    println("E\n")
+    names.sort()                       //FOR ORDER THE LIST
+    for (name in names) {
+        println(name)
+    }
+
+}
+
