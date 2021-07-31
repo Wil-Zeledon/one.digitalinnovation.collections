23990204896703adb4d50a5e2a29522ecc6c9e09
Wil-Zeledon <wzeledon@ug.uchile.cl>
2021-07-31 18:17:05 -0300

    Created int array test class

--- src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -15,4 +15,18 @@ fun main() {
     println("The Average Salary is: ${salarios.average()}")
     println("The Salaries greater than 2,250.0 is/are: ${salarios.filter {it>2500.0}}")
 
+    println("\nC")
+    println(salarios.count{it in 2000.0..5000.0 })
+
+    println("\nD")
+    println(salarios.find { it == 2250.0 })
+
+    println("\nE")
+    println("Do you found the quantity of salary about 500.0?: " + salarios.find { it == 500.0 })
+
+    println("\nF")
+    println("Do you found the quantity of salary about 1000.0?: " + salarios.any { it == 1000.0 })
+
+    println("\nG")
+    println("Do you found the quantity of salary about 500.0?: " + salarios.any { it == 500.0 })
 }
