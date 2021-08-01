a0d916e547b84ac50fdb8acd966bdc6063273109
Wil-Zeledon <wzeledon@ug.uchile.cl>
2021-07-31 22:56:30 -0300

    Created int array test class

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -5,17 +5,17 @@ import java.nio.file.Files.find
 fun main() {
     val Josue = funcionary(name = "Josue", salary = 2000.0, ContractType = "CLT")
     val Pedro = funcionary(name = "Pedro", salary = 1500.0, ContractType = "PJ")
-    val María = funcionary(name = "María", salary = 4000.0, ContractType = "CLT")
+    val Maria = funcionary(name = "Maria", salary = 4000.0, ContractType = "CLT")
 
-    val funcionaries = listOf(Josue, Pedro, María)
+    val funcionaries = listOf(Josue, Pedro, Maria)
 
     println("\n")
     funcionaries.forEach { println(it)}
 
 
     println("\n")
-    println("The funacionary is:")
-    println(funcionaries.find{it.name == "María" })
+    println("The funcionary is:")
+    println(funcionaries.find{it.name == "Maria" })
 
     println("\n")
     funcionaries.sortedBy { it.salary }.forEach { println (it)}                       //FOR ORDER THE LIST WITH sortedBy
